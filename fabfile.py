4994b905ae13816aafae9917d7b782cd38707fc9
logickee <logickee@gmail.com>
2019-08-02 14:41:59 +0800

    init commit,

--- fabfile.py
+++ fabfile.py
@@ -16,6 +16,10 @@ total_cpu_threads = multiprocessing.cpu_count()
 CWD = os.path.dirname(__file__)
 print("CWD: %s" % CWD)
 
+def get_hostname():
+    hostname = local('hostname', capture=True)
+    return hostname.strip()
+
 def upload_to_transfer_sh():
     iso_filename = 'linuxium-v5.3-rc2-origin.iso'
     output_iso = os.path.join(CWD,iso_filename)
@@ -43,14 +47,19 @@ def helloworld():
             pass
         else:
             print(yellow("iso file not exist, download from ubuntu"))
-            local('wget http://releases.ubuntu.com/19.04/ubuntu-19.04-desktop-amd64.iso -O ./origin/origin.iso')
+            if get_hostname() in['logic-ThinkCentre-M73', 'logic-ThinkPad-X201']
+                local('wget http://ftp.cuhk.edu.hk/pub/Linux/ubuntu-releases/19.04/ubuntu-19.04-desktop-amd64.iso -O ./origin/origin.iso')
+            else:
+                local('wget http://releases.ubuntu.com/19.04/ubuntu-19.04-desktop-amd64.iso -O ./origin/origin.iso')
 
         local('rm -rf ./destination/*.iso')
         local('rm -rf .isorespin.sh.lock')
         local('rm -rf ./linuxium*.iso')
         local('sudo rm -rf ./isorespin')
         print("clear directory done")
-        # local('wget http://ftp.cuhk.edu.hk/pub/Linux/ubuntu-releases/19.04/ubuntu-19.04-desktop-amd64.iso -O ./origin/origin.iso')
         local('./build.sh  ./origin/origin.iso -c bionicbeaver')
         print("isprespin done")
         upload_to_transfer_sh()
+
+def temp():
+    print('%s' % get_hostname())
