ff35870cb200631814bad351cbd08ac1fc972da5
logickee <logickee@gmail.com>
2019-08-02 17:42:04 +0800

    update,

--- apt_install.sh
+++ apt_install.sh
@@ -13,3 +13,16 @@ sudo apt-get update
 sudo apt-get install -y code # or code-insiders
 
 which code
+
+sudo add-apt-repository -y ppa:daniruiz/flat-remix
+sudo add-apt-repository -y ppa:dhor/myway
+sudo add-apt-repository -y ppa:js-reynaud/kicad-5.1
+sudo add-apt-repository -y ppa:freecad-maintainers/freecad-stable
+sudo add-apt-repository -y ppa:kritalime/ppa
+sudo add-apt-repository -y ppa:maarten-fonville/android-studio
+sudo add-apt-repository -y ppa:morphis/anbox-support
+sudo add-apt-repository -y ppa:nilarimogard/webupd8
+sudo add-apt-repository -y ppa:rvm/smplayer
+sudo add-apt-repository -y ppa:snwh/ppa
+sudo add-apt-repository -y ppa:ubuntuhandbook1/apps
+sudo add-apt-repository -y ppa:lyzardking/ubuntu-make
