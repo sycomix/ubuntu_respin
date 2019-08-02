850e80d05aa5e4c35a9430c0bc7c45b7983fe1a9
logickee <logickee@gmail.com>
2019-08-02 18:08:28 +0800

    update,

--- apt_install.sh
+++ apt_install.sh
@@ -13,3 +13,9 @@ sudo apt-get update
 sudo apt-get install -y code # or code-insiders
 
 which code
+
+# mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
