876238cc8ec064489366ee84c78e6e69e8ab7a8a
logickee <logickee@gmail.com>
2019-08-02 15:14:11 +0800

    update,

--- apt_install.sh
+++ apt_install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y vscode
+# https://code.visualstudio.com/docs/setup/linux
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
